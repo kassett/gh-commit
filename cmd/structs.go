cfe2bdf20fff5d60416487d1cceaf02c3c9ba7cc
Samuel Chai <121340503+kassett@users.noreply.github.com>
2025-04-15 15:45:09 -0400

    Actual commits using the tool

--- cmd/structs.go
+++ cmd/structs.go
@@ -9,3 +9,34 @@ type BranchDescriptionResponse struct {
 		SHA string `json:"sha"`
 	} `json:"commit"`
 }
+
+type ShaResponse struct {
+	Sha string `json:"sha"`
+}
+
+type BlobInfo struct {
+	Path string  `json:"path"`
+	Mode string  `json:"mode"`
+	Type string  `json:"type"`
+	Sha  *string `json:"sha"`
+}
+
+type PrResponse struct {
+	Url    string `json:"url"`
+	Number int    `json:"number"`
+}
+
+type PrRequest struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+	Head  string `json:"head"`
+	Base  string `json:"base"`
+}
+
+type LabelResponse struct {
+	Name string `json:"name"`
+}
+
+type LabelRequest struct {
+	Labels []string `json:"labels"`
+}
